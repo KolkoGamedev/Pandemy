596c8f199cb857b297d32ae4c3bf236550261723
Syberian <mixux98@gmail.com>
2020-10-09 15:24:05 +0200

    comments

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -9,14 +9,14 @@ public class IntroManager : MonoBehaviour
 {
     [SerializeField] private FogManager fogManager = null;
     [SerializeField] private TMP_Text welcomeText = null;
-    [SerializeField] private List<Sentence> introDialogue = null;
+    [SerializeField] private List<Sentence> introDialogue = null;        // List of texts
     [SerializeField] private TMP_Text gameTitleText = null;
     [SerializeField] private GameObject menuHolder = null;
     private TypeWriter _typeWriter;
     
     private void Awake()
     {
-        fogManager.OnFogFadeIn += StartSequence;
+        fogManager.OnFogFadeIn += StartSequence;                        // Start sequence of texts right after fog goes in
     }
 
     private void Start()
@@ -29,14 +29,14 @@ public class IntroManager : MonoBehaviour
         StartCoroutine(Sequence());
     }
 
-    private IEnumerator Sequence()
+    private IEnumerator Sequence()                                     // Coroutine that shows every text line with delay from inspector
     {
         for (int i = 0; i < introDialogue.Count; i++)
         {
             yield return new WaitUntil(() => _typeWriter.canWrite);
             _typeWriter.TypewriteSentence(introDialogue[i].text, welcomeText, introDialogue[i].delay);
         }
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3);                            // Wait 3 seconds after last text to fade out
         StartCoroutine(FadeOutText());
     }
     
@@ -51,11 +51,11 @@ public class IntroManager : MonoBehaviour
             welcomeText.color = new Color(textColor.r, textColor.b, textColor.b, elapsedTime);
             yield return null;
         }
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f);                        // Wait 1 second after fadeout to start showing menu
         ShowMenu();
     }
 
-    private IEnumerator FadeInText(TMP_Text text)
+    private IEnumerator FadeInText(TMP_Text text)                    
     {
         float elapsedTime = 0f;
         float desiredTime = 1f;
