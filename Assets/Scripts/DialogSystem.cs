53ffd44ebfac206cd64a3265019b82ed18959563
Syberian <mixux98@gmail.com>
2020-10-13 21:10:07 +0200

    starting dialogue system

---
+++ Assets/Scripts/DialogSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text narratorTextField = null;
+
+    private FogManager _fogManager;
+    private TypeWriter _tw;
+    private void Awake()
+    {
+        _tw = FindObjectOfType<TypeWriter>();
+    }
+
+    private void Start()
+    {
+        _fogManager = FindObjectOfType < FogManager > ();
+        _fogManager.OnFogFadeIn += StartGame;
+    }
+
+    public void NarratorSpeech(string sentence, float delay)
+    {
+        _tw.TypewriteSentence(sentence, narratorTextField, delay);
+    }
+
+    private void StartGame()
+    {
+        NarratorSpeech("You wake up from long rest", 1f);
+    }
+    
+}
