2a3bf7e31058e58aa9dbaa767596b212f93574e5
Dawid <dawidbadura111@gmail.com>
2021-02-02 20:39:05 +0100

    Minigames (Bomba i labirynt)
    Zrobiłem sam bez pomocy żadnych kolegów dwie super minigry. Jedna polega na wyjebaniu bomby w luft a druga w czyms innym pozdro

---
+++ Assets/Scripts/LabiryntMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabiryntMovement : MiniGame
+{
+    [SerializeField] private Canvas gameCanvas;
+    [SerializeField] private GameObject spawnPoint;
+    Vector2 pos;
+    bool isClicked = false;
+
+    private void Awake()
+    {
+ 
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name.Equals("EndPoint"))
+        {
+            isClicked = false;
+            base.FinishGame();
+        }
+        else
+        {
+            isClicked = false;
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+    void OnMouseDown()
+    {
+        isClicked = true;
+    }
+    private void Update()
+    {
+        if(isClicked)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(gameCanvas.transform as RectTransform, Input.mousePosition, gameCanvas.worldCamera, out pos);
+            transform.position = gameCanvas.transform.TransformPoint(pos);
+        }    
+    }
+}
