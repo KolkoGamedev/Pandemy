e9c9bec076db86302cd7c23019280394bd0aeed3
Syberian <syberianbusiness@gmail.com>
2021-02-02 15:16:35 +0100

    dsada

--- Assets/Scripts/Labirynt.cs
+++ Assets/Scripts/Labirynt.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Data.Util;
 using UnityEngine;
 
 public class Labirynt : MiniGame
 {
-
+    private void Awake()
+    {
+        base.FinishGame();
+    }
 }
